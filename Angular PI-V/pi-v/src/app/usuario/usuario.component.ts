056be44574640168a861992cc79b9e6007834e18
Pedro Henrique Machado Da Silva <144813383+PedroHenriqueMS@users.noreply.github.com>
2024-06-10 22:41:19 -0300

    PI-V Apresentacao

--- Angular PI-V/pi-v/src/app/usuario/usuario.component.ts
+++ Angular PI-V/pi-v/src/app/usuario/usuario.component.ts
@@ -17,46 +17,46 @@ export class UsuarioComponent implements OnInit {
   lista: Usuario[] = [];
   obj: Usuario = new Usuario();
 
-  constructor(private api: UsuarioService) {}
+  constructor(private api: UsuarioService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.consultar();
   }
 
   consultar() {
     this.api.consultar()
-    .toPromise()
-    .then
-    ((res: any) => {
-      this.lista = res;
-    });
+      .toPromise()
+      .then
+      ((res: any) => {
+        this.lista = res;
+      });
   }
-  
+
   adicionar() {
     this.api.adicionar(this.obj)
-    .toPromise()
-    .then((usuario: any) => {
-      alert(usuario.userNome + " foi adicioando(a) com sucesso!");
-      this.consultar();
-    });
+      .toPromise()
+      .then((usuario: any) => {
+        alert(usuario.userNome + " foi adicioando(a) com sucesso!");
+        this.consultar();
+      });
   }
 
   excluir(id: number) {
     this.api.excluir(id)
-    .toPromise()
-    .then( () => {
-      alert("Usuario excluida com sucesso!");
-      this.consultar();
-    });
+      .toPromise()
+      .then(() => {
+        alert("Usuario excluida com sucesso!");
+        this.consultar();
+      });
   }
 
   alterar() {
     this.api.alterar(this.obj.userId, this.obj)
-    .toPromise()
-    .then( () => {
-      alert("Usuario alterada com sucesso!");
-      this.consultar();
-    });
+      .toPromise()
+      .then(() => {
+        alert("Usuario alterada com sucesso!");
+        this.consultar();
+      });
   }
 
   carregarDados(u: Usuario) {
@@ -66,5 +66,30 @@ export class UsuarioComponent implements OnInit {
   limparDados() {
     this.obj = new Usuario();
   }
-  
+
+  // mostrarSenha(button) {
+  //   var td = button.parentNode.previousElementSibling;
+  //   var senha = td.getAttribute('data-senha');
+  //   var input = document.createElement('input');
+  //   input.type = 'password';
+  //   input.value = senha;
+  //   td.innerHTML = '';
+  //   td.appendChild(input);
+  //   button.onclick = function () {
+  //     ocultarSenha(this, senha);
+  //   };
+  //   button.innerText = 'Ocultar senha';
+  // }
+
+  // ocultarSenha(button, senha) {
+  //   var td = button.parentNode.previousElementSibling;
+  //   var input = td.firstElementChild;
+  //   td.innerHTML = '******';
+  //   td.setAttribute('data-senha', senha);
+  //   button.onclick = function () {
+  //     mostrarSenha(this);
+  //   };
+  //   button.innerText = 'Mostrar senha';
+  // }
+
 }
