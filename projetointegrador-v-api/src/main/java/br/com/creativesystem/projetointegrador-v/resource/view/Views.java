d98d7834ed142940858e2b0b5e1f792637d9c81c
Pedro Henrique Machado Da Silva <144813383+PedroHenriqueMS@users.noreply.github.com>
2024-06-10 22:42:26 -0300

    Delete projetointegrador-v-api directory

--- projetointegrador-v-api/src/main/java/br/com/creativesystem/projetointegrador-v/resource/view/Views.java
+++
@@ -1,9 +0,0 @@
-package br.com.creativesystem.projetointegradoriv.resource.view;
-
-public interface Views {
-    public interface Base {
-    }
-
-    public interface Detalhado extends Base {
-    }
-}
